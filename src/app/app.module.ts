255182cd1ee05433aec8068fb33a85e5c0219fdd
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2021-11-08 09:49:50 -0300

    update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MapaComponent } from './pages/mapa/mapa.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MapComponent } from './components/map/map.component';
+import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { MaterialModule } from './module/material.module';
+import { AddMarkerComponent } from './components/map/add-marker/add-marker.component';
 
 @NgModule({
-	declarations: [AppComponent, MapaComponent],
-	imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+	declarations: [
+		AppComponent,
+		HeaderComponent,
+		MapComponent,
+		HomeComponent,
+		AddMarkerComponent,
+	],
+	imports: [
+		BrowserModule,
+		MaterialModule,
+		ReactiveFormsModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		LeafletModule,
+	],
 	providers: [],
 	bootstrap: [AppComponent],
 })
